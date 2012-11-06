c19f1b74bd82a2c088ef7b013799126866b81214
Michael Gatto <mgatto@lisantra.com>
2012-11-05 19:00:14 -0700

    * Remove HashInflator and simplify syntax to extract data in templates.

--- roomr.pl
+++ roomr.pl
@@ -19,8 +19,8 @@ get '/buildings' => sub {
     my $self = shift;
 
     my $rs = $self->db->resultset('Building');
-    $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
-    my @buildings = $rs->all;
+    #$rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+    my @buildings = $rs->all();
 
     $self->respond_to(
         json  => {json => [@buildings]},
@@ -33,50 +33,53 @@ get '/buildings' => sub {
 
 # List rooms
 # @TODO should display visual cues about room
-get '/building/:name/rooms' => sub {
+get '/building/:slug/rooms' => sub {
     my $self = shift;
-    my $building_name = $self->stash('name');
+    my $slug = $self->stash('slug');
 
     my $rs = $self->db->resultset('Room');
-    $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
-    my @all_rooms = $rs->all;
+    # $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+    # all() is what actually executes the search; 'In list context, all() is
+    # called implicitly on the resultset, thus returning a list of row objects
+    # instead. To avoid that, use "search_rs".'
+    my @rooms_by_building = $rs->search({ 'building.slug' => $slug }, { join => 'building' })->all;
 
     $self->respond_to(
-        json  => {json => [@all_rooms]},
+        json  => {json => [@rooms_by_building]},
         html => {
             template => 'rooms',
-            rooms => [@all_rooms],
+            rooms => [@rooms_by_building],
         }
     );
-
 };
 
 # Room details
-get '/room/:name' => sub {
+get '/room/:slug' => sub {
     my $self = shift;
     my $room_name  = $self->stash('name');
 
-    my @this_rooms_reservations = $self->db->resultset('Reservations')->all;
+    my $rs = $self->db->resultset('Reservations');
+    my @this_rooms_reservations = $rs->all();
 
     $self->render('reservations', reservations => @this_rooms_reservations);
 };
 
 # reserves a room for a block of time
-post '/room/:name/reserve' => sub {
+post '/room/:slug/reserve' => sub {
     my $self = shift;
     my $name  = $self->stash('name');
 
 };
 
 # Edit a reservation
-post '/room/:name/edit' => sub {
+post '/room/:slug/reservation/edit' => sub {
     my $self = shift;
     my $room_name  = $self->stash('name');
 
 };
 
 # Cancel a reservation
-post '/room/:name/cancel' => sub {
+post '/room/:slug/reservation/cancel' => sub {
     my $self = shift;
     my $room_name  = $self->stash('name');
 
